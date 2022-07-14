2e77009e056828542a6cf8739274abcf0337aefa
lorenzo venneri <lorenzo.venneri@smc.it>
2022-07-14 10:02:50 +0200

    Add: spinner loading

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,9 @@
-import React, { useContext } from "react";
+import React from "react";
 import datas from "./Data";
-import CardHeader from "@mui/material/CardHeader";
 import Avatar from "@mui/material/Avatar";
 import { NavLink } from "react-router-dom";
 import "./home.css";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 import {
   Grid,
@@ -15,86 +13,27 @@ import {
   ListItemAvatar,
   ListItemText,
   styled,
-  Typography,
 } from "@mui/material";
+import style from "styled-components";
 import { useQuery } from "react-query";
-import { ApiContext } from "./api";
 import { createEmptyStorage, createInMemoryApi } from "./inMemomoryApi";
 
 const Demo = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
 }));
 
-function getData() {
-  return new Promise((resolve) =>
-    // setTimeout(
-    // () =>
-    //   resolve([
-    //     { id: 1, user: "Paolo" },
-    //     { id: 2, user: "Francesco" },
-    //     { id: 3, user: "Mattia" },
-    //     { id: 4, user: "Luca" },
-    //   ]),
-    // 1000
-    // )
-    () => {
-      console.log("inizio");
-      const stored = localStorage.getItem("userAndPosts");
-      const storage = stored ? JSON.parse(stored) : createEmptyStorage();
-      const api = createInMemoryApi(storage);
-      const getData = async () => {
-        const data = await api.getUsers();
-        return data;
-      };
-      return getData;
-    }
-  );
-
-  // const { data: list } = useQuery(
-  //   ["/api/datasource/v2/suggestion-category", { tenantId }],
-  //   async ({ queryKey: [path, parameters] }) => {
-  //     if (USE_MOCK) return mockSuggestionCategories;
-  //     return (await client.getDatasourceSuggestionCategories()).filter(
-  //       (suggestionCategory) => suggestionCategory.tenantId === tenantId,
-  //     );
-  //   },
-  // );
-
-  // {
-  //   const stored = localStorage.getItem("userAndPosts");
-  //   const storage = stored ? JSON.parse(stored) : createEmptyStorage();
-  //   const api = createInMemoryApi(storage);
-  //   const getData = async () => {
-  //     const data = await api.getUsers();
-  //     return data;
-  //   };
-  //   console.log(getData);
-  // }
-  // );
-}
-
 const Home = () => {
   const [dense, setDense] = React.useState(false);
 
-  const [users, setUsers] = React.useState<string[]>(
-    JSON.parse(localStorage.getItem("Registre") || "[]") || []
-  );
+  // const [users, setUsers] = React.useState<string[]>(
+  //   JSON.parse(localStorage.getItem("Registre") || "[]") || []
+  // );
   const [entry, setEntry] = React.useState(false);
   const [theme, setTheme] = React.useState("light-mode");
-  // const { status, data, error } = useQuery("user", getData);
-
-  // const stored = localStorage.getItem("userAndPosts");
-  // const storage = stored ? JSON.parse(stored) : createEmptyStorage();
-
-  // const localStorageApi = createLocalStorageApi("userAndPosts");
-  // const test2 = inMemoryApi.getUsers();
 
   const stored = localStorage.getItem("userAndPosts");
   const storage = stored ? JSON.parse(stored) : createEmptyStorage();
   const api = createInMemoryApi(storage);
-
-  const test2 = api.getUsers();
-  console.log(test2);
 
   const cambiaTema = () => {
     if (theme === "light-mode") {
@@ -105,26 +44,44 @@ const Home = () => {
   };
 
   const { status, data } = useQuery(["user"], async () => {
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 5001))
+    );
     return await api.getUsers();
   });
 
-  console.log("cad" + JSON.stringify(data));
+  // React.useEffect(() => {
+  //   document.documentElement.className = theme;
+  // }, [theme]);
 
-  React.useEffect(() => {
-    document.documentElement.className = theme;
-  }, [theme]);
+  // React.useEffect(() => {
+  //   localStorage.setItem("Registre", JSON.stringify(users));
+  // }, [users]);
 
-  React.useEffect(() => {
-    localStorage.setItem("Registre", JSON.stringify(users));
-  }, [users]);
-  console.log("pre error");
+  const Spinner = style.div`
+    border: 10px solid #1966FF;
+    border-top: 10px white solid;
+    border-radius: 50%;
+    height: 30px;
+    width: 30px;
+    animation: spin 2s linear infinite;
+    margin-left:160px;
+    margin-top:130px;
+    @keyframes spin {
+      0% {
+        transform: rotate(0deg);
+      }
+
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  `;
 
   function stampaNewUserd() {
-    console.log("adz" + JSON.stringify(data));
     const newData = data?.map((data) => {
       return (
         <>
-          {console.log(data.name)}
           <Demo>
             <List dense={dense}>
               <NavLink to="/homepage" style={{ textDecoration: "none" }}>
@@ -139,7 +96,12 @@ const Home = () => {
                     <Avatar>{/* <FolderIcon /> */}</Avatar>
                   </ListItemAvatar>
 
-                  <ListItemText primary={data.name} />
+                  <ListItemText
+                    primary={data.name}
+                    onClick={() => {
+                      localStorage.setItem("Loggato", data.name);
+                    }}
+                  />
                 </ListItem>
               </NavLink>
             </List>
@@ -167,7 +129,12 @@ const Home = () => {
                 {" "}
                 <Avatar>{/* <FolderIcon /> */}</Avatar>
               </ListItemAvatar>
-              <ListItemText primary={data.nick} />
+              <ListItemText
+                primary={data.nick}
+                onClick={() => {
+                  localStorage.setItem("Loggato", data.nick);
+                }}
+              />
             </ListItem>
           </NavLink>
         </List>
@@ -198,7 +165,10 @@ const Home = () => {
       </div>
 
       {status === "loading" ? (
-        <span>Loading...</span>
+        <span>
+          {" "}
+          <Spinner />
+        </span>
       ) : status === "error" ? (
         <span>error</span>
       ) : (
@@ -206,18 +176,11 @@ const Home = () => {
       )}
 
       <div className="grid-container">
-        <Grid item xs={12} md={6}>
-          {/* {stampaNewUserd()} */}
-        </Grid>
         <Grid item xs={12} md={6}></Grid>
-        {newData}
+        {/* {newData} */}
       </div>
     </>
   );
 };
-function RecuperaDati() {
-  const inMemoryApi = useContext(ApiContext);
-  const usersQuery = useQuery(["user", {}], async () => inMemoryApi.getUsers());
-}
 
 export default Home;
